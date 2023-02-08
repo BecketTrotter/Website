fb8cc9dad352b9221c1ea4ec043bc7a2029fe58b
Henry Trotter <hbtrotter38@gmail.com>
2023-02-08 09:21:40 -0500

    mobile friendlier?

--- src/Project.tsx
+++ src/Project.tsx
@@ -9,7 +9,7 @@ const Project: React.FC<Props> = ({ projectName, src }) => {
   return (
     <div>
       {(src.endsWith('.mov') || src.endsWith('.mp4')) ? (
-        <video preload={"auto"}  autoPlay muted loop src={src} style={{ width: '100%', height: '100%' }} />
+        <video preload={"auto"} playsInline={true} autoPlay muted loop src={src} style={{ width: '100%', height: '100%' }} />
       ) : (
         <img src={src} alt={projectName} style={{ width: '100%' }} />
       )}

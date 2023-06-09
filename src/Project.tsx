4af270f041c78bd6086d1d4377b4fbdf00fd49b2
Henry Trotter <hbtrotter38@gmail.com>
2023-06-09 14:24:23 -0400

    mobile ?

--- src/Project.tsx
+++ src/Project.tsx
@@ -8,11 +8,7 @@ interface Props {
 const Project: React.FC<Props> = ({ projectName, src }) => {
   return (
     <div>
-      {(src.endsWith('.mov') || src.endsWith('.mp4')) ? (
         <video preload={"auto"} playsInline={true} autoPlay muted loop src={src} style={{ width: '100%', height: '100%' }} />
-      ) : (
-        <img src={src} alt={projectName} style={{ width: '100%' }} />
-      )}
     </div>
   );
 };
